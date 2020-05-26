00fa23b04b50f5b7be93c3a70954a1d3e84789ed
dangkhoapham31 <khoaphamdang31@gmail.com>
2020-05-26 21:43:33 +1000

    change color of signup and login page, create navigation bar on homepage, start designing UI of homepage

--- Project Source Code/app/src/main/java/com/example/myapplication/Homepage.java
+++ Project Source Code/app/src/main/java/com/example/myapplication/Homepage.java
@@ -1,32 +1,86 @@
 package com.example.myapplication;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class Homepage extends AppCompatActivity {
+public class Homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        drawer = findViewById(R.id.drawer_layout);
         setSupportActionBar(toolbar);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmet_container, new Fragment_homepage()).commit();
+            navigationView.setCheckedItem(R.id.nav_view);
+        }
     }
 
+    /*public void logout (View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish(); }*/
+    public  void user_profile (View view){
+        startActivity(new Intent(getApplicationContext(), UserProfile.class));
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            //case R.id.home:
+             //   getSupportFragmentManager().beginTransaction().replace(R.id.fragmet_container, new Fragment_homepage()).commit();
+             //   break;
+            //case R.id.expedition:
+             //   getSupportFragmentManager().beginTransaction().replace(R.id.fragmet_container, new Fragment_expedition()).commit();
+             //   break;
+            //case R.id.blog:
+            //    startActivity(new Intent(HomePage.this, Mylog.class));
+            //    break;
+            //case R.id.contact:
+             //   startActivity(new Intent(HomePage.this, Contact.class));
+             //   break;
+            //case R.id.donate:
+            //    getSupportFragmentManager().beginTransaction().replace(R.id.fragmet_container, new Fragment_donation()).commit();
+            //    break;
+            case R.id.signout:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
+
